88c140fa44e24f1229c8771c486c1832f1762a84
“weisong” <450428539@qq.com>
2019-11-20 18:03:19 +0800

    1120

--- src/main/java/com/java/controller/LoginController.java
+++ src/main/java/com/java/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.java.controller;
 import com.java.pojo.UserInfo;
 import com.java.service.LoginService;
 import com.java.util.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,14 +23,16 @@ import java.util.Map;
  * author: ws
  * time: 2019/11/19 16:27
  */
+@Api(value = "FP登录", description = "用户登录")
 @Controller
 @ResponseBody
+@RequestMapping("/fplogin/")
 
 public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @RequestMapping("/fplogin")
+    @RequestMapping(value = "fplogin" , method = RequestMethod.GET)
     public UserInfo login(String userName, String userPassword){
         UserInfo userInfo1 = loginService.login(userName, userPassword);
 //        System.out.println("=========================");
@@ -36,13 +42,17 @@ public class LoginController {
         return null;
     }
 
-    @RequestMapping(value = "/fplogin2", method = RequestMethod.GET)
+    @ApiOperation(value = "登录方法", notes = "用户登录")
+    @ApiParam(value = "userName", required = true)
+    @RequestMapping(value = "/fplogin2", method = RequestMethod.POST)
     public Response login(UserInfo userInfo, HttpServletRequest request){
         UserInfo userInfo1 = loginService.login(userInfo.getUserName(), userInfo.getUserPassword());
         if(userInfo1 != null){
             Map map = new HashMap();
-            HttpSession session = request.getSession();
+            HttpSession session = request.getSession(); //获取Session对象
             session.setAttribute(userInfo1.getUuid(),userInfo1);
+            Object obj = session.getAttribute(userInfo1.getUuid());
+            System.out.println("obj(userInfo1)对象为" + obj);
             map.put("uuid", userInfo1.getUuid());   //用户唯一识别号
             map.put("userRole", userInfo1.getUserRole());
             map.put("userName", userInfo1.getUserName());
@@ -50,8 +60,6 @@ public class LoginController {
         }
         return Response.error("用户不存在或用户名/密码错误！");
     }
-
-
 
 
 }
